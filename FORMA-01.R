38528c480a6f19e0f4b317b99767c79319f7bea4
Simon <simon.montenegro@usach.cl>
2022-01-21 19:55:40 -0300

    Se descarga el enunciado y se inicia la prueba 19:49

---
+++ FORMA-01.R
@@ -0,0 +1,50 @@
+# Se especifica y almacena la ruta del directorio de la base de datos.
+dir <- "~/../Desktop/IME/PEP_2_IME/PEP-2-IME"
+base <- "Datos PEP 2.csv"
+arch <- file.path(dir, base)
+
+# Se realiza la lectura de los datos, se escpecifica el formato de codificaci???n UTF-8
+datos <- read.csv2(arch, fileEncoding = "UTF-8")
+
+# --------------- PREGUNTA 1 ---------------
+# Lord Vader desea saber si los niveles de exigencia con que los instructores de las 
+# diferentes divisiones evalúan a los nuevos soldados son similares, por lo que le ha 
+# solicitado estudiar si existen diferencias significativas en el promedio de la 
+# evaluación realizada por el instructor entre las distintas divisiones. El Lord Sith 
+# ha sido muy claro al solicitar un reporte de aquellas divisiones en las que se observen 
+# diferencias.
+
+# Las hipótesis a formular son:
+# H0: No existen diferencias significativas en el promedio de la evaluación realizada por
+#     el instructor entre las distintas divisiones.
+# HA: Sí existe diferencias significativas en el promedio de la evaluación realizada por
+#     el instructor entre las distintas divisiones.
+
+# Las condiciones a verificar:
+# 1.
+# 2.
+# 3.
+# 4.
+
+
+# CONCLUSIONES
+#
+
+
+
+
+# --------------- PREGUNTA 2 ---------------
+
+# Las hipótesis a formular son:
+# H0:
+# HA: 
+
+# Las condiciones a verificar:
+# 1.
+# 2.
+# 3.
+# 4.
+
+
+# CONCLUSIONES
+#
