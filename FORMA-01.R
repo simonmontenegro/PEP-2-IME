e9fd63962186da8f6be749745bf489d612c0e9ad
Simon <simon.montenegro@usach.cl>
2022-01-21 21:59:33 -0300

    Ultimo commit, faltó tiempo

--- FORMA-01.R
+++ FORMA-01.R
@@ -22,47 +22,47 @@ datos <- read.csv2(arch, fileEncoding = "UTF-8")
 # Lord Vader desea saber si los niveles de exigencia con que los instructores de las 
 # diferentes divisiones evalúan a los nuevos soldados son similares, por lo que le ha 
 # solicitado estudiar si existen diferencias significativas en el promedio de la 
-# evaluaciÃ³n realizada por el instructor entre las distintas divisiones. El Lord Sith 
+# evaluación realizada por el instructor entre las distintas divisiones. El Lord Sith 
 # ha sido muy claro al solicitar un reporte de aquellas divisiones en las que se observen 
 # diferencias.
 
 # Dado que se solicita evaluar diversas divisiones bajo la evaluacion del instructor, es que
-# se debe planear un abordaje mediante ANOVA de una vÃ­a para muestras independientes, esto pues 
+# se debe planear un abordaje mediante ANOVA de una ví­a para muestras independientes, esto pues 
 # se considera un estudio para mas de dos muestras (divisiones en este caso), considerando una 
-# variable independiente cuyos niveles definen los grupos que se estÃ¡n comparando.
+# variable independiente cuyos niveles definen los grupos que se están comparando.
 
-#En torno al enunciado y las solicitudes de Lord Vader, se plantea la prueba de hipÃ³tesis que
+#En torno al enunciado y las solicitudes de Lord Vader, se plantea la prueba de hipótesis que
 # representa el problema.
 
-# Las hipÃ³tesis a formular son:
-# H0: No existen diferencias significativas en el promedio de la evaluaciÃ³n realizada por
+# Las hipótesis a formular son:
+# H0: No existen diferencias significativas en el promedio de la evaluación realizada por
 #     el instructor entre las distintas divisiones.
-# HA: SÃ­ existe diferencias significativas en el promedio de la evaluaciÃ³n realizada por
+# HA: Sí existe diferencias significativas en el promedio de la evaluación realizada por
 #     el instructor entre las distintas divisiones.
 
-#Se filtran los datos de interÃ©s del problema, es decir, division y evaluacion del instructor,
-# en este caso tambiÃ©n se incluye 'id' para facilitar los procedimientos de mÃ¡s adelante.
+#Se filtran los datos de interés del problema, es decir, division y evaluacion del instructor,
+# en este caso también se incluye 'id' para facilitar los procedimientos de más adelante.
 datos_filtrados <- datos %>% select(id, division, eval_instructor)
 
-#Para el procedimiento de ANOVA de una vÃ­a para muestras independientes se deben verificar algunas
+#Para el procedimiento de ANOVA de una vía para muestras independientes se deben verificar algunas
 # condiciones previas para trabajar.
 # Las condiciones a verificar:
 # 1.La escala con que se mide la variable dependiente tiene las propiedades de una escala de intervalos 
 # iguales. 
 #    La escala con que se mide la variable dependiente tiene las propiedades de una escala de
-#    de intervalos iguales, dado que la variable a medir se encuentra en forma numÃ©rica para todas 
-#    las observaciones. (Se asume como premisa que el instructor realiza la asignaciÃ³n de puntaje
+#    de intervalos iguales, dado que la variable a medir se encuentra en forma numérica para todas 
+#    las observaciones. (Se asume como premisa que el instructor realiza la asignación de puntaje
 #    dentro de una escala de intervalos iguales, es decir, utiliza la misma escala independiente de
-#    la divisiÃ³n del recluta o del recluta en sÃ­)
+#    la división del recluta o del recluta en sí)
 # 
-# 2.Las k muestras son obtenidas de manera aleatoria e independiente desde la(s) poblaciÃ³n(es) de origen.
+# 2.Las k muestras son obtenidas de manera aleatoria e independiente desde la(s) población(es) de origen.
 #    Se asume que las observaciones para las k muestras obtenidas se recopilan y seleccionan de manera
-#    totalmente aleatoria y de manera independiente desde la poblaciÃ³n para cada divisiÃ³n.
+#    totalmente aleatoria y de manera independiente desde la población para cada división.
 #   
-# 3.Se puede suponer razonablemente que la(s) poblaciÃ³n(es) de origen sigue(n) una distribuciÃ³n normal.
-#   Para verificar esta condicion, se realiza el grÃ¡fico Q-Q
+# 3.Se puede suponer razonablemente que la(s) población(es) de origen sigue(n) una distribución normal.
+#   Para verificar esta condicion, se realiza el gráfico Q-Q
 
-#   ComprobaciÃ³n de normalidad .
+#   Comprobación de normalidad .
 g <-   ggqqplot(datos_filtrados, x = "eval_instructor", y = "division", color = "division") 
 g <-   g + facet_wrap (~ division )
 g <-   g + rremove("x.ticks") + rremove("x.text") 
@@ -70,19 +70,19 @@ g <-   g + rremove("y.ticks") + rremove("y.text")
 g <-   g + rremove("axis.title")
 print(g)
 
-#   Es correcto afirma que en base al grÃ¡fico, existen algunos valores atÃ­picos, pero son muy leves.
+#   Es correcto afirma que en base al gráfico, existen algunos valores atípicos, pero son muy leves.
 #   Sin embargo, debido a estos valores, es necesario ser cautelosos al realizar la prueba, por tanto,
-#   se utilizarÃ¡ un nivel de significaciÃ³n igual a 0.01.
+#   se utilizará un nivel de significación igual a 0.01.
 
 alfa <- 0.01
 
 # 4.Las k muestras tienen varianzas aproximadamente iguales.
-#   Para esto, se hace uso de ezAnova(), la cual permite realizar tambiÃ©n la prueba de homocedasticidad
-#   de Levene, la cual permite dar respuesta a la condiciÃ³n.
+#   Para esto, se hace uso de ezAnova(), la cual permite realizar también la prueba de homocedasticidad
+#   de Levene, la cual permite dar respuesta a la condición.
 
-# Se establecen las hipÃ³tesis para el test de Levine.
+# Se establecen las hipótesis para el test de Levine.
 # H0: Las varianzas de las k muestras son iguales.
-# HA: Al menos una de las muestras tiene varianza diferente a alguna de las demÃ¡s. 
+# HA: Al menos una de las muestras tiene varianza diferente a alguna de las demás. 
 
 cat("\n\nProcedimiento ANOVA usando ezANOVA\n\n")
 pruebaEzAnova <- ezANOVA(data = datos_filtrados,
@@ -93,36 +93,36 @@ pruebaEzAnova <- ezANOVA(data = datos_filtrados,
                         return_aov = TRUE)
 print(pruebaEzAnova)
 
-#GrÃ¡fico del tama?o del efecto.
+#Gráfico del tama?o del efecto.
 g2 <- ezPlot(data = datos_filtrados , 
               dv = eval_instructor,
               wid = id, 
               between = division,
-              y_lab = "Promedio de la evaluaciÃ³n realizada por el instructor entre las distintas divisiones", 
+              y_lab = "Promedio de la evaluación realizada por el instructor entre las distintas divisiones", 
               x = division)
 print(g2)
 
 #En torno a estos resultados del test de homogeneidad de las varianzas, se tiene 
-# un p-value igual a 0.5035376, que a su vez supera al nivel de significaciÃ³n estipulado,
-# con esto, se falla al rechazar la hipÃ³tesis nula, por lo que se verifica que las varianzas de las
-# k muestras son iguales, es decir, se cumple la cuarta condiciÃ³n.
+# un p-value igual a 0.5035376, que a su vez supera al nivel de significación estipulado,
+# con esto, se falla al rechazar la hipótesis nula, por lo que se verifica que las varianzas de las
+# k muestras son iguales, es decir, se cumple la cuarta condición.
 
 # Conclusiones
-# Como se viÃ³ anteriormente al aplicar ezAnova(), el resultado del p-value en torno a la situaciÃ³n corresponde
+# Como se vió anteriormente al aplicar ezAnova(), el resultado del p-value en torno a la situación corresponde
 # a p ~ 2.575512e-107, es decir, un valor muy (mucho) por debajo del nivel de significancia establecido, por lo que
-# se rechaza la hipÃ³tesis nula en favor de la hipÃ³tesis alternativa, se puede asegurar con un 99% de confianza
-# que sÃ­ existen diferencias significativas en el promedio de la evaluaciÃ³n realizada por el instructor entre las 
-# distintas divisiones. Por tanto, es necesario realizar un anÃ¡lisis Post-Hoc para saber en quÃ© divisiones se 
+# se rechaza la hipótesis nula en favor de la hipótesis alternativa, se puede asegurar con un 99% de confianza
+# que sí existen diferencias significativas en el promedio de la evaluación realizada por el instructor entre las 
+# distintas divisiones. Por tanto, es necesario realizar un anÃ¡lisis Post-Hoc para saber en qué divisiones se 
 # encuentran esas diferencias.
-# Esto es muy evidente si analiza el grÃ¡fico del tamaÃ±o del poder, donde "Cavetrooper" y "Spacetrooper" se desmarcan
-# totalmente en el grÃ¡fico.
+# Esto es muy evidente si analiza el gráfico del tamañoo del poder, donde "Cavetrooper" y "Spacetrooper" se desmarcan
+# totalmente en el gráfico.
 
 # Dado que el enunciado menciona que "El Lord Sith ha sido muy claro al solicitar un reporte de aquellas divisiones 
 # en las que se observen diferencias" se hace totalmente necesario obtener aquellas divisiones donde existen dichas
 # diferencias (debemos recordar que ANOVA corresponde a una prueba de tipo OMNIBUS, esto significa que no menciona/detalla
 # donde se encuentran las diferencias, solo dice si existen o no).
 
-# Con lo analizado, se procede a realizar un procedimiento Post-Hoc HSD Tukey, por el nivel de precisiÃ³n en la
+# Con lo analizado, se procede a realizar un procedimiento Post-Hoc HSD Tukey, por el nivel de precisión en la
 # seleccion de diferencias significativas.
 
 #Procedimiento ANOVA con aov ().
@@ -138,12 +138,12 @@ post_hoc <- TukeyHSD(pruebaAnova,
 print(post_hoc)
 
 # Conclusiones
-# En base a los resultados obtenidos, es posible notar que existen diferencias muy significativas entre la divisiÃ³n 
-# Spacetrooper y las demÃ¡s, como tambiÃ©n entre la divisiÃ³n Cavetrooper y las demÃ¡s (esto se infiere a partir del anÃ¡lisis
-# del p-value conseguido para cada una de las diferencias, donde las divisiones mencionadas presentan un p-value Ã­nfimo).
+# En base a los resultados obtenidos, es posible notar que existen diferencias muy significativas entre la división 
+# Spacetrooper y las demÃ¡s, como tambiÃ©n entre la división Cavetrooper y las demás (esto se infiere a partir del análisis
+# del p-value conseguido para cada una de las diferencias, donde las divisiones mencionadas presentan un p-value í­nfimo).
 # Por otro lado, el resto de divisiones presentan un p-value mucho mayor que el nivel de signficancia 0.01, esto se
-# verifica a partir del grÃ¡fico del tamaÃ±o del poder, donde las seis divisiones estÃ¡n centradas casi en 650 y presentan
-# leves diferencias entre sÃ­. PodrÃ­a destacarse que la divisiÃ³n "Shoretrooper" presenta una diferencia que se escapa, sin
+# verifica a partir del grÃ¡fico del tamaño del poder, donde las seis divisiones están centradas casi en 650 y presentan
+# leves diferencias entre sÃ­. PodrÃ­a destacarse que la división "Shoretrooper" presenta una diferencia que se escapa, sin
 # embargo, esta diferencia no llega a ser significativa.
 
 # A partir de lo concluido, se debe notificar al Lord Sith de que aquellas divisiones que presentan diferencias 
@@ -232,17 +232,8 @@ correlaciones <- round(cor(x = nuevos_datos, method = "pearson"), 3)
 #Al analizar las correlaciones con la variable de respuesta (es_clon)
 # se puede ver que presentan valores que se relacionan de buena manera con la variable a predecir.
 
-#Los residuos deben ser independientes entre sí.
-# Comprobación de independencia de los residuos.
-cat("\nPrueba de Durbin-Watson para autocorrelaciones.")
-cat("entre errores:\n")
-print(durbinWatsonTest(modelo))
-#Se cumple con la independencia de los residuos, puesto que el p-value obtenido
-# está muy por sobre el nivel de significancia, por lo que se puede concluir que 
-# en efecto los residuos son independientes.
-
 #Comprobación de multicolinealidad
-vifs <- vif(modelo_final)
+vifs <- vif(modelo)
 cat("\nVerificar la multicolinealidad:\n")
 cat("- VIFs: \n")
 print(vifs)
@@ -250,6 +241,33 @@ cat("- Tolerancias:\n")
 print(1 / vifs)
 cat("- VIF medio:", mean(vifs), "\n")
 
+#En el caso de la multicolinealidad, los datos recabados sugieren que el modelo
+# podría estar sesgado, puesto que mas de la mitad de los predictores resultaron en una
+# tolerancia menor a 0.4, donde además el VIF promedio supera por mucho el valor 2.5,  
+# lo que aumenta la preocupación en este aspecto.
+modelo_corregido <- update(modelo, . ~ .  -fuerza, -precision)
+
+#ComprobaciÃ³n de multicolinealidad
+vifs <- vif(modelo_corregido)
+cat("\nVerificar la multicolinealidad:\n")
+cat("- VIFs: \n")
+print(vifs)
+cat("- Tolerancias:\n")
+print(1 / vifs)
+cat("- VIF medio:", mean(vifs), "\n")
+
+#En así como se corrige el modelo, se quitan los ultimos dos predictores añadidos,
+# obteniendose así un resultado favorable que comprueba la condición de multicoli-
+# nealidad.
+
+#Los residuos deben ser independientes entre sí.
+# Comprobación de independencia de los residuos.
+cat("\nPrueba de Durbin-Watson para autocorrelaciones.")
+cat("entre errores:\n")
+print(durbinWatsonTest(modelo_corregido))
+#Se cumple con la independencia de los residuos, puesto que el p-value obtenido
+# está muy por sobre el nivel de significancia, por lo que se puede concluir que 
+# en efecto los residuos son independientes.
 
 # Revisar casos con distancia de Cook mayor a uno.
 resultado <- data.frame(predicted.probabilities = fitted(modelo))
@@ -261,9 +279,57 @@ cat ("- - - - - - - - - - -- - - - - - - - - - - - - - - - - - - - -- - - - - - 
 print(rownames(entrenamiento[sospechosos2, ]))
 
 
+#Se crea el conjunto de entrenamiento para la validación.
+nuevos_datos$es:clon <- factor(nuevos_datos$es_clon)
+nRL <- nrow(nuevos_datos)
+n_entrenamientoRL <- floor(0.8 * nRL)
+muestraRL <- sample.int(n=nRL, size=n_entrenamientoRL, replace = F)
+entrenamientoRL <- nuevos_datos[muestraRL, ]
+pruebaRL <- nuevos_datos[-muestraRL, ]
+
+modelo_RL <- glm(es_clon ~ velocidad + agilidad, family = binomial(link = "logit"),
+                 data = entrenamientoRL)
+print(summary(modelo_RL))
+
+#Se revisan las curvas Roc
+comparar_umbral <- function(x){
+  umbral <- 0.5
+  if(x >= umbral){
+    return(1)
+  }else{
+    return(0)
+  }
+} 
+
+#Se evalua el modelo RL con el conjunto de entrenamiento.
+probs_RL_entrenamiento <- predict(modelo_RL, entrenamientoRL, type = "response")
+preds_RL_entrenamiento <- sapply(probs_RL_entrenamiento, comparar_umbral)
+preds_RL_entrenamiento <- factor(preds_RL_entrenamiento, levels = levels(nuevos_datos[["es_clon"]]))
+
+#Se evalua el modelo RL con el conjunto de prueba
+probs_RL_prueba <- predict(modelo_RL, pruebaRL, type = "response")
+preds_RL_prueba <- sapply(probs_RL_prueba, comparar_umbral)
+preds_RL_prueba <- factor(preds_RL_prueba, levels = levels(nuevos_datos[["es_clon"]]))
+
+
+
+#Se revisa la curva ROC
+roc_RL_entrenamiento <- roc(entrenamientoRL[["es_clon"]], probs_RL_entrenamiento)
+plot(roc_RL_entrenamiento)
+
+roc_RL_prueba <- roc(pruebaRL[["es_clon"]], probs_RL_prueba)
+plot(roc_RL_prueba)
+
+#Evaluar modelo para prueba
+matrizRL_prueba <- confusionMatrix(preds_RL_prueba, pruebaRL[["es_clon"]])
+print(matrizRL_prueba)
+
+#Evaluar modelo para entrenamiento
+matrizRL_entrenamiento <- confusionMatrix(preds_RL_entrenamiento, entrenamientoRL[["es_clon"]])
+print(matrizRL_entrenamiento)
 
 # Conclusiones
-#
+# No alcancé
 
 
 # --------------- PREGUNTA 3 ---------------
@@ -273,12 +339,14 @@ print(rownames(entrenamiento[sospechosos2, ]))
 # en estudio. Indique cuáles serÃ­an las variables involucradas en su ejemplo (con sus respectivos niveles) y las hipótesis
 # nula y alternativa a contrastar.
 
-# La directiva de la Universidad de Santiago de Chile desea conocer la preferencia de sus estudiantes con respecto al retorno
+# La directiva de la Universidad de Santiago de Chile desea conocer la satisfacción de sus estudiantes con respecto al retorno
 # a clases presenciales de acuerdo a algunas medidas que ha tomado el establecimiento con respecto a convatir el coronavirus,
 # brindandoles la confianza de volver a clases a sus estudiantes. Específicamente, se desea evaluar las variables de distanciamiento 
 # físico (en la sala de clases), la sanitización (qué tan bien es realizado este y cada cuánto tiempo), y la seguridad (verificar 
 # la temperatura de todas las personas que ingresan y si estas tienen sus vacunas al día). Esta evaluación, lo hace con ayuda 
 # de la escala Likert de 6 puntos, donde 1 indica "estoy muy en desacuerdo", y el 6 indica "estoy muy de acuerdo". 
+# Se destaca que que las variables independientes son "Distanciamiento", "Sanitización" y "Seguridad", mientras que la variable
+# dependiente corresponde al nivel de satisfacción que posee cada estudiantes (en una escala del 1 al 6).
 
 # Por tanto, se establecen las siguientes hipótesis:
 # H0: Las variables evaluadas tienen resultados positivos (mayor o igual a 5).
